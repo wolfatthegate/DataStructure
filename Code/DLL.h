1feab69a21a890b54b226c3d4dffac25a6b8b572
折棒 <orekihoutarou1993@gmail.com>
2018-11-07 18:25:37 -0500

    modify dll

--- Code/DLL.h
+++ Code/DLL.h
@@ -1,12 +1,12 @@
 //
-// Created by KylinA1 on 2018/11/5.
+// Created by KylinA1 on 2018/11/7.
 //
 
-#ifndef INC_2017F_DLL_H
-#define INC_2017F_DLL_H
+#ifndef ALL_DLL_H
+#define ALL_DLL_H
 
 #include <iostream>
-template <typename T>
+template<typename T>
 class dnode{
 public:
     T data;
@@ -23,50 +23,50 @@ private:
 
 public:
     Itr():current(nullptr){};
-    Itr(dnode<T>* ptr):current(ptr){};
+    Itr(dnode<T>* ptr):current(ptr){};// not const
 
-    Itr<T>& operator++(){current=current->next; return *this;}//pre
+    Itr<T>& operator++(){
+        current=current->next;
+        return *this;
+    }//pre
     Itr<T> operator++(int){
         Itr<T> result(current);
         current=current->next;
         return result;
-    }
-    Itr<T>& operator--(){current=current->prev; return *this;}//pre
-    Itr<T> operator--(int){
-        Itr<T> result(current);
-        current=current->prev;
-        return result;
-    }
+    }//post
 
     dnode<T>* operator->(){
         return current;
     }
 
-    const dnode<T>* operator->()const{return current;}
+    const dnode<T>* operator->()const{
+        return current;
+    }
 
-    bool operator==(dnode<T>* ptr){return current==ptr;};
-    bool operator!=(dnode<T>* ptr){return current!=ptr;};
+    bool operator==(const dnode<T>* ptr){return current==ptr;};
+    bool operator!=(const dnode<T>* ptr){return current!=ptr;};
+
+
 };
 
 template<typename T>
 class List{
 private:
-    dnode<T> *beginning,*ending;
+    dnode<T> * beginning,*ending;
 
 public:
     List():beginning(nullptr),ending(nullptr){};
     ~List(){
-        dnode<T> *temp;
+        dnode<T> *temp=beginning;
         while(beginning!= nullptr){
-            temp=beginning;
             beginning=beginning->next;
             delete temp;
+            temp=beginning;
         }
     }
-
     List(const List<T>& actual){
-        beginning= nullptr;
         dnode<T>* temp=actual.beginning;
+        beginning= nullptr;
         while(temp!= nullptr){
             if(beginning== nullptr){
                 beginning=new dnode<T>(temp->data);
@@ -80,74 +80,91 @@ public:
         }
     }
 
-    void insertAfter(const T& item, Itr<T> ptr){
-        dnode<T>* temp=new dnode<T>(item);
+    void insertAfter(const T& item,Itr<T> ptr){
+      if(beginning== nullptr){
+          beginning=new dnode<T>(item);
+          ending=beginning;
+      }else{
+          if(ptr==ending){
+              ending->next=new dnode<T>(item);
+              ending->next->prev=ending;
+              ending=ending->next;
+          }else{
+              dnode<T>* temp=new dnode<T>(item);
+              ptr->next->prev=temp;
+              temp->next=ptr->next;
+              ptr->next=temp;
+              temp->prev=ptr.operator->();
+          }
+      }
+    }
+
+    void insertBefore(const T& item, Itr<T> ptr){
         if(beginning== nullptr){
-            beginning=temp;
-            ending=temp;
+            beginning=new dnode<T>(item);
+            ending=beginning;
         }else{
-            temp->next=ptr->next;
-            temp->prev=ptr.operator->();// return current;
-
-            if(ptr == ending){
-                ending->next=temp;
-                ending=temp;
+            if(ptr==beginning){
+                beginning->prev=new dnode<T>(item);
+                beginning->prev->next=beginning;
+                beginning=beginning->next;
             }else{
-                ptr->next->prev=temp;
-                ptr->next=temp;
+                dnode<T>* temp=new dnode<T>(item);
+                ptr->prev->next=temp;
+                temp->prev=ptr->prev;
+                temp->next=ptr.operator->();
+                ptr->prev=temp;
             }
+
         }
     }
 
 
-    Itr<T> begin(){
-        Itr<T> result(beginning);
-        return result;
-    }
 
-    const Itr<T> begin()const{
-        Itr<T> result(beginning);
-        return result;
-    }
-
-    Itr<T> end(){
-        Itr<T> result(ending);
-        return result;
-    }
-
-    const Itr<T> end()const{
-        Itr<T> result(ending);
-        return result;
-    }
-
-    void remove(Itr<T> ptr) {
-        if (ptr == beginning) {
-            beginning = beginning->next;
-        } else {
-            ptr->prev->next = ptr->next;
+    void remove(Itr<T> ptr){
+        if(ptr==beginning){
+            beginning=beginning->next;
+        }else{
+            ptr->prev->next=ptr->next;
         }
 
-
-        if (ptr == ending) {
-            ending = ending->prev;
-        } else {
-            ptr->next->prev = ptr->prev;
+        if(ptr==ending){
+            ending=ending->prev;
+        }else{
+            ptr->next->prev=ptr->prev;
         }
-
         delete ptr.operator->();
     }
 
-   // --- for test---//
+    Itr<T> begin(){
+        return Itr<T>(beginning);
+    }
 
-    void Print(){
+    const Itr<T> begin()const{
+        return Itr<T>(beginning);
+    }
+
+    Itr<T> end(){
+        return Itr<T>(ending);
+    }
+    const Itr<T> end()const{
+        return Itr<T>(ending);
+    }
+
+   /////for test
+    void PrintList(){
         dnode<T>* temp=beginning;
         while(temp!= nullptr){
-            std::cout<< temp->data<<" ";
+            std::cout<<temp->data<<" ";
             temp=temp->next;
         }
         std::cout<<std::endl;
     }
+
 };
 
 
-#endif //INC_2017F_DLL_H
+
+
+
+#endif //ALL_DLL_H
